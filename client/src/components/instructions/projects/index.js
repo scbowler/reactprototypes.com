2e50ce1a9dbb0fcfc96c40746970232d9a33d3df
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-09-11 15:54:55 -0700

    Mboutique part 1 project complete

--- client/src/components/instructions/projects/index.js
+++ client/src/components/instructions/projects/index.js
@@ -1,10 +1,21 @@
 import Guess from './guess-game';
+import MBoutique from './mboutique';
+
+// const name must be camelCased and the 'to' property must match using hyphens
 
 export const guessingGame = {
     component: Guess,
     title: 'Guessing Game',
     subtitle: '001',
     text: 'Create a simple number guessing game using React',
-    to: 'guessing-game',
+    to: 'guessing-game'
+};
+
+export const mboutiqueHome = {
+    component: MBoutique,
+    title: 'MBoutique | Part 1',
+    subtitle: '002',
+    text: 'Build the home page of the MBoutique website using React',
+    to: 'mboutique-home',
     badge: ''
 };
