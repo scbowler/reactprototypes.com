8fec2133a8d5aa8d635f5709abd5a5f1b0e205b8
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-09-15 14:48:11 -0700

    Added To Do List project, updated some code comments

---
+++ client/src/components/instructions/projects/todo-list/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from '../../../cards/instruction-card';
+import Code from '../../../code';
+import Cc from '../../../code_component';
+import examples from './examples';
+
+export default function(){ 
+    return (
+        <div>
+            <Card title="1" subtitle="Something fun">
+                <ol>
+                    <li>Coming Soon!</li>
+                </ol>
+            </Card>
+        </div>
+    )
+}
