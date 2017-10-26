e23c561a216f721ea24e81646518ff87c15b1622
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-10-25 23:12:20 -0700

    Completed initial styles and images prototype instructions

---
+++ client/src/components/instructions/prototypes/react-styles-images/example/image_data.js
@@ -0,0 +1,73 @@
+import puppy1 from './images/puppy1.jpeg';
+import puppy2 from './images/puppy2.jpeg';
+import puppy3 from './images/puppy3.jpg';
+import puppy4 from './images/puppy4.jpeg';
+import puppy5 from './images/puppy5.jpeg';
+import puppy6 from './images/puppy6.jpeg';
+import puppy7 from './images/puppy7.jpeg';
+import puppy8 from './images/puppy8.jpeg';
+
+export default [
+    {
+        src: puppy1,
+        style: {
+            left: '5%',
+            transform: 'rotate(-10deg)'
+        }
+    },
+    {
+        src: puppy2,
+        style: {
+            left: '40%',
+            transform: 'rotate(6deg)'
+        }
+    },
+    {
+        src: puppy3,
+        style: {
+            right: '15%',
+            transform: 'rotate(-9deg)'
+        }
+    },
+    {
+        src: puppy4,
+        style: {
+            left: '8%',
+            top: '28%',
+            transform: 'rotate(8deg)'
+        }
+    },
+    {
+        src: puppy5,
+        style: {
+            left: '37%',
+            top: '25%',
+            transform: 'rotate(-12deg)'
+        }
+    },
+    {
+        src: puppy6,
+        style: {
+            right: '12%',
+            top: '26%',
+            transform: 'rotate(10deg)'
+        }
+    },
+    {
+        src: puppy7,
+        style: {
+            left: '25%',
+            top: '48%',
+            transform: 'rotate(-5deg)'
+
+        }
+    },
+    {
+        src: puppy8,
+        style: {
+            right: '25%',
+            top: '52%',
+            transform: 'rotate(16deg)'
+        }
+    }
+];
